e9904ccf4051432ba008d383771825a72ceaef89
Ethan <ethanny2@gmail.com>
2020-02-12 00:13:46 -0500

    Restructured directory, cleaned sourced files, finished most of readme, added in shields. Next make a gif demonstration.

--- decode.m
+++ decode.m
@@ -1,39 +1,26 @@
-    s= imread('C:\Users\Student\Desktop\soohon-e-338-proj7\stego-project\encodedImg.bmp'); %The bmp image with the hidden stego image
-    k=1;
-    height= size(s,1); 
-    width= size(s,2);
-    %Max number of bits is 25, so 25*8 bits neeeded 200
-    m= 25*8;
-    % blanks(n) returns a 1-by-n array of space characters.
-   % b=zeros(m);
-    
-   
-    %Loop in all pixels and get lsb. 
-    
-    for i=1:height
-        for j=1:width
-            if k<=m
-                b(k)=mod(double(s(i,j)),2);
-                k=k+1;
-            end
+s= imread('[YOUR_PATH_HERE]\encodedImg.bmp');
+k=1;
+height= size(s,1); 
+width= size(s,2);
+%Max number of bits is 25, so 25*8 bits neeeded 200
+m= 25*8;   
+%Loop in all pixels and get lsb.     
+for i=1:height
+    for j=1:width
+        if k<=m
+            b(k)=mod(double(s(i,j)),2);
+            k=k+1;
         end
     end
-    %b is the extracted secret message as a bit string
-    %print to the terminal screen 
-   
-   %Now next we will convert from binary to a string that humans can see
-   %c= char(bin2dec(reshape(b,8,[]).'))
-   %disp(c);
-   %fid = fopen('C:\temp\coursedata\stego_decoded.txt','wt');
-   %fwrite(fid, c ,'char');
-   %fclose(fid);
-    
-    vec = b;
-    bin = [128 64 32 16 8 4 2 1];
-    vec = vec(:);
-    binary_martrix = reshape(vec,8,[]);
-    text = char(bin * binary_martrix);
-    %disp(text);    
-    id = fopen('C:\Users\Student\Desktop\soohon-e-338-proj7\stego-project\decodedMessage.txt','w');
-    fwrite(id,text);
-    fclose(id);
+end
+%b is the extracted secret message as a bit string
+%print to the terminal screen    
+%Now next we will convert from binary to a string that humans can see    
+vec = b;
+bin = [128 64 32 16 8 4 2 1];
+vec = vec(:);
+binary_martrix = reshape(vec,8,[]);
+text = char(bin * binary_martrix);   
+id = fopen('[YOUR_PATH_HERE]\decodedMessage.txt','w');
+fwrite(id,text);
+fclose(id);
--- encode.m
+++ encode.m
@@ -1,77 +1,44 @@
 
-    c = imread('C:\Users\Student\Desktop\soohon-e-338-proj7\stego-project\earth.bmp');  %The path for the grayscale bmp cover image
-    id = fopen('C:\Users\Student\Desktop\soohon-e-338-proj7\stego-project\secret_message.txt', 'r');
-    spec = '%c';
-    message = fscanf(id,spec);
-%   disp(message); %testing if the string was actually retrived
-
-    %testing if bmp image is color or greyscale
-    %isColor = ndims(c);
-    %testMe = size(c,3);
-   
-    m = length(message) * 8;
-    %message2= strtrim(message2);
-    %disp(message2);
-   
-   ascii = uint8(message); %Message in Ascii int form
-  %  disp(ascii);
-    binary = transpose(dec2bin(ascii,8));
-    %disp(binary);
-    binary = binary(:);
-    LSB=0;
-   num = length(binary);
-   b = zeros(num,1); %b is a vector of bits
-   
-    
-    for k = 1:num
-        if(binary(k) == '1')
-            %disp(binary(k));
+c = imread('[YOUR_PATH_HERE]\images\earth.bmp');  %The path for the grayscale bmp cover image
+id = fopen('[YOUR_PATH_HERE]\secret_message.txt', 'r');
+spec = '%c';
+message = fscanf(id,spec);  
+m = length(message) * 8;   
+% Message in Ascii int form
+ascii = uint8(message);
+binary = transpose(dec2bin(ascii,8));
+binary = binary(:);
+LSB=0;
+num = length(binary);
+b = zeros(num,1); %b is a vector of bits       
+for k = 1:num
+    if(binary(k) == '1')
             b(k) = 1;
-        else
+    else
             b(k) = 0;
-        end
     end
- 
-  %  disp(b);
+end
 
-  %  casted = uint8(message2);
-    
- %   string_arr  = int2bin(casted,25);
-   % disp(string_arr); %second param is length?
-    
-    
-    s = c;
-    height = size(c,1);
-    width = size(c,2);
-   
- 
-    k = 1;
-    for i = 1 : height
-        %    if(k>25)
-	       %         break;
-	        % end;
-        for j = 1 : width
-         
-             LSB = mod(double(c(i,j)), 2);
-            if (k>m || LSB == b(k))
-                s(i,j) = c(i,j);
-            else
-               s(i,j)=c(i,j)+b(k) -LSB;
-            end
-            k = k + 1;
-             
+s = c;
+height = size(c,1);
+width = size(c,2);
+k = 1;
+
+for i = 1 : height
+    for j = 1 : width
+        LSB = mod(double(c(i,j)), 2);
+        if (k>m || LSB == b(k))
+            s(i,j) = c(i,j);
+        else
+            s(i,j)=c(i,j)+b(k) -LSB;
         end
+         k = k + 1;     
     end
+end
       
-    
-    
-    
-    
-    
-     %Write image
-   % imwrite(s, 'C:\temp\coursedata\stego.bmp');
-     disp('Done encoding message');
-    imwrite(s,'C:\Users\Student\Desktop\soohon-e-338-proj7\stego-project\encodedImg.bmp','bmp'); %write the new image with the hidden image in it to 's' or disk.
+% Write image
+disp('Done encoding message');
+imwrite(s,'[YOUR_PATH_HERE]\encodedImg.bmp','bmp'); %write the new image with the hidden image in it to 's' or disk.
 
 
     
